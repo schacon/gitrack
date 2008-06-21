bad58e1823f33936271a95a2485b5893753718dc
Scott Chacon <schacon@gmail.com>
2008-06-21 16:10:56 -0700

    added gitrack Rack script

---
+++ gitrack.rb
@@ -0,0 +1,22 @@
+require 'rack/lib/rack'
+require 'simplegit/lib/simplegit'
+
+class GitRack
+  def call(env)
+    [200, {'Content-type' => 'text/plain'},
+      git_files_list]
+  end
+  
+  def git_files_list
+    SimpleGit.new('.git').log
+  end
+end
+
+app = Rack::Builder.new {
+  use Rack::CommonLogger, STDERR
+  run GitRack.new
+}.to_app
+
+server = Rack::Handler::WEBrick
+options = {:Port => 9292, :Host => "0.0.0.0"}
+server.run app, options
